78c12cf44d035eb7288493867c3b5dbee67479fb
jasongreer12 <jasong@rae-corp.com>
2025-04-16 20:54:15 -0500

    cleaned up files

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express, { Application, Request, Response } from 'express';
 import sql from 'mssql';
 import dbConfig from './config/dbConfig';
 import getTodayDowntime from './routes/getDowntime';
-import insertDowntime from './routes/insertDowntime';
 import getMetrics from './routes/getMetrics';
 import updateOpDetail from './routes/updateOpDetail';
 
@@ -10,22 +9,11 @@ const app: Application = express();
 app.use(express.json());
 
 
-/*
-  Note: for project, i should be displaying plant duration for selected day, total rock crushed for day, total plant downtime for day, 
-        downtime event, true plant availibility, plant availibility % for day. 
-
-
-
-*/
-
-
-
 app.use(express.static("public"));
 // connect to SQL database
 sql.connect(dbConfig)
   .then((pool) => {
     console.log('Connected to Microsoft SQL Server');
-    // Optionally attach the connection pool to app.locals
     app.locals.db = pool;
   })
   .catch((err) => {
@@ -46,7 +34,6 @@ app.get('/api/beltdetails', async (req: Request, res: Response) => {
 });
 
 app.use('/api', getTodayDowntime);
-app.use('/api', insertDowntime);
 app.use('/api', getMetrics);
 app.use('/api', updateOpDetail);
 
